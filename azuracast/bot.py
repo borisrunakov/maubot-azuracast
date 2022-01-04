c484961c9acfa7db164e83458d2e089cec8818f2
boris runakov <borisrunakov@gmail.com>
2022-01-04 10:09:16 +0200

    init commit

---
+++ azuracast/bot.py
@@ -0,0 +1,64 @@
+import logging
+from typing import Type
+from maubot import Plugin, MessageEvent
+from maubot.handlers import command
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
+from azuracast.radio import RadioInfo
+
+logger = logging.getLogger(__name__)
+
+
+class Config(BaseProxyConfig):
+    def do_update(self, helper: ConfigUpdateHelper) -> None:
+        helper.copy("radio_api_address")
+        helper.copy("radio_idx")
+
+
+class AzuracastBot(Plugin):
+    config: Config
+
+    async def start(self) -> None:
+        await super().start()
+        self.on_external_config_update()
+        self.radio = RadioInfo(self.config["radio_api_address"], self.config["radio_idx"])
+
+    def on_external_config_update(self) -> None:
+        self.config.load_and_update()
+
+    @classmethod
+    def get_config_class(cls) -> Type[BaseProxyConfig]:
+        return Config
+
+    @command.new("radio", aliases=['r'])
+    async def radio(self) -> None:
+        pass
+
+    @radio.subcommand("listeners", aliases=['ls'], help="Current listeners counter.")
+    async def handler(self, evt: MessageEvent) -> None:
+        await evt.mark_read()
+        self.radio.update()
+        await evt.reply(self.radio.get_listeners(), allow_html=True)
+
+    @radio.subcommand("now", help="Playing now.")
+    async def handler(self, evt: MessageEvent) -> None:
+        await evt.mark_read()
+        self.radio.update()
+        await evt.reply(self.radio.get_now_playing(), allow_html=True)
+
+    @radio.subcommand("next", help="Playing next.")
+    async def handler(self, evt: MessageEvent) -> None:
+        await evt.mark_read()
+        self.radio.update()
+        await evt.reply(self.radio.get_next_playing(), allow_html=True)
+
+    @radio.subcommand("streamer", aliases=['str'], help="Who's the streamer yo.")
+    async def handler(self, evt: MessageEvent) -> None:
+        await evt.mark_read()
+        self.radio.update()
+        await evt.reply(self.radio.get_streamer(), allow_html=True)
+
+    @radio.subcommand("all", aliases=['a'], help="Print all available radio information.")
+    async def handler(self, evt: MessageEvent) -> None:
+        await evt.mark_read()
+        self.radio.update()
+        await evt.reply(self.radio.get_all(), allow_html=True)
---
+++ azuracast/radio.py
@@ -0,0 +1,117 @@
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
+
+
+class RadioInfo:
+    def __init__(self, url, station_num=0):
+        self.url = url
+        self.station_num = station_num
+        self.radio_station_name = None
+        self.radio_station_description = None
+        self.current_listeners = None
+        self.unique_listeners = None
+        self.total_listeners = None
+        self.streamer_name = None
+        self.now_playing_playlist = None
+        self.now_playing_song_title = None
+        self.now_playing_song_artist = None
+        self.now_playing_song_album = None
+        self.next_playing_playlist = None
+        self.next_playing_song_title = None
+        self.next_playing_song_artist = None
+        self.next_playing_song_album = None
+
+    def update(self):
+        logger.debug(f"Getting info for station {self.station_num}")
+        radio_info = self.get_json_data()
+        self.update_info(radio_info)
+
+    def get_json_data(self):
+        r = requests.get(self.url)
+        return r.json()[self.station_num]
+
+    def shape_data(self, radio_info):
+        for key, value in radio_info.items():
+            if isinstance(value, dict):
+                self.shape_data(value)
+            else:
+                if value is None:
+                    radio_info[key] = {}
+                if value == '':
+                    radio_info[key] = '-'
+        return radio_info
+
+    def update_info(self, radio_info):
+        self.radio_station_name = self.traverse_dict_node(radio_info, ['station', 'name'])
+        self.radio_station_description = self.traverse_dict_node(radio_info,
+                                                                 ['station', 'description'])
+        self.current_listeners = self.traverse_dict_node(radio_info, ['listeners', 'current'])
+        self.unique_listeners = self.traverse_dict_node(radio_info, ['listeners', 'unique'])
+        self.total_listeners = self.traverse_dict_node(radio_info, ['listeners', 'total'])
+        self.streamer_name = self.get_streamer_name(radio_info)
+        self.now_playing_playlist = self.traverse_dict_node(radio_info,
+                                                            ['now_playing', 'playlist'])
+        self.now_playing_song_title = self.traverse_dict_node(radio_info,
+                                                              ['now_playing', 'song', 'title'])
+        self.now_playing_song_artist = self.traverse_dict_node(radio_info,
+                                                               ['now_playing', 'song', 'artist'])
+        self.now_playing_song_album = self.traverse_dict_node(radio_info,
+                                                              ['now_playing', 'song', 'album'])
+        self.next_playing_playlist = self.traverse_dict_node(radio_info,
+                                                             ['playing_next', 'playlist'])
+        self.next_playing_song_title = self.traverse_dict_node(radio_info,
+                                                               ['playing_next', 'song', 'title'])
+        self.next_playing_song_artist = self.traverse_dict_node(radio_info,
+                                                                ['playing_next', 'song', 'artist'])
+        self.next_playing_song_album = self.traverse_dict_node(radio_info,
+                                                               ['playing_next', 'song', 'album'])
+
+    @staticmethod
+    def traverse_dict_node(dictionary, node_keys):
+        value = None
+        for key in node_keys:
+            if dictionary is None:
+                dictionary = {}
+            dictionary = dictionary.get(key)
+            value = dictionary
+            if value == '' or value is None:
+                value = '-'
+        return value
+
+    @staticmethod
+    def get_streamer_name(radio_info):
+        streamer_name = radio_info["live"].get("streamer_name", "-")
+        if not streamer_name:
+            return "AutoDJ"
+        else:
+            return streamer_name
+
+    def get_now_playing(self):
+        return f"<h4>playlist now</h4>\
+                {self.now_playing_playlist}<br>\
+                <h4>song</h4>\
+                <b>title :</b> {self.now_playing_song_title}<br>\
+                <b>artist :</b> {self.now_playing_song_artist}<br>\
+                <b>album :</b> {self.now_playing_song_album}<br>"
+
+    def get_next_playing(self):
+        return f"<h4>playlist next</h4>\
+                {self.next_playing_playlist}<br>\
+                <h4>song</h4>\
+                <b>title :</b> {self.next_playing_song_title}<br>\
+                <b>artist :</b> {self.next_playing_song_artist}<br>\
+                <b>album :</b> {self.next_playing_song_album}<br>"
+
+    def get_listeners(self):
+        return "<h4>listeners</h4>**current:** {} **unique:** {} **total:** {}<br>".format(
+            self.current_listeners,
+            self.unique_listeners,
+            self.total_listeners)
+
+    def get_streamer(self):
+        return "<h4>streamer</h4> {}".format(self.streamer_name)
+
+    def get_all(self):
+        return self.get_listeners() + self.get_now_playing() + self.get_next_playing() + self.get_streamer()
